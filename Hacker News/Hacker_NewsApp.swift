b294dfab4a764344aa1a8c574ae971c74d844f02
Badal Aryal <badal.aryal@gmail.com>
2024-02-09 15:56:52 +0545

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Badal  Aryal on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
